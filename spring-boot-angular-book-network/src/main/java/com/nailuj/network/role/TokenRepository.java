6d59e4d57b8637ceef68df355e6b7e2a1eac2f1c
Julian Mercado <julianmercadomeza@gmail.com>
2024-06-05 03:15:29 -0400

    Added services and repositories to project

---
+++ spring-boot-angular-book-network/src/main/java/com/nailuj/network/role/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.nailuj.network.role;
+
+import com.nailuj.network.user.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findByToken(String token);
+
+}
