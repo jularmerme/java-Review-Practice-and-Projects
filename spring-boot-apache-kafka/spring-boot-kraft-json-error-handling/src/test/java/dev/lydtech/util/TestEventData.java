0016a9be568fa6fc58b5eec1c96d6df938c96e9d
Julian Mercado <julianmercadomeza@gmail.com>
2024-07-08 14:17:42 -0400

    Added finished files for kraft broker using json serialization

---
+++ spring-boot-apache-kafka/spring-boot-kraft-json-error-handling/src/test/java/dev/lydtech/util/TestEventData.java
@@ -0,0 +1,16 @@
+package dev.lydtech.util;
+
+import dev.lydtech.message.OrderCreated;
+
+import java.util.UUID;
+
+public class TestEventData {
+
+    public static OrderCreated buildOrderCreatedEvent(UUID orderId, String item) {
+        return OrderCreated.builder()
+                .orderId(orderId)
+                .item(item)
+                .build();
+    }
+
+}
