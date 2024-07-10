67f48277f92e7b79adabdb58a0e0e07ca06e3542
Julian Mercado <julianmercadomeza@gmail.com>
2024-07-09 23:08:18 -0400

    added files for projects files about unit testing

---
+++ spring-boot-unit-testing/01.junit-basic-example/src/test/java/com/luv2code/junitdemo/DemoUtilTest.java
@@ -0,0 +1,32 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoUtilTest {
+
+    @Test
+    void testEqualAndNotEquals() {
+
+        DemoUtils demoUtils = new DemoUtils();
+
+        assertEquals(6, demoUtils.add( 2, 4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+
+    }
+
+    @Test
+    void testNullAndNotNull() {
+
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "Junit Tests";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+
+}
---
+++ spring-boot-unit-testing/02.junit-lifecycle-methods/src/test/java/com/luv2code/junitdemo/DemoUtilTest.java
@@ -0,0 +1,44 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoUtilTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test Method");
+    }
+
+
+    @Test
+    void testEqualAndNotEquals() {
+
+        System.out.println("Running test: testEqualAndNotEquals");
+
+        assertEquals(6, demoUtils.add( 2, 4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+
+    }
+
+    @Test
+    void testNullAndNotNull() {
+
+        System.out.println("Running test: testNullAndNotNull");
+
+        String str1 = null;
+        String str2 = "Junit Tests";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+
+}
