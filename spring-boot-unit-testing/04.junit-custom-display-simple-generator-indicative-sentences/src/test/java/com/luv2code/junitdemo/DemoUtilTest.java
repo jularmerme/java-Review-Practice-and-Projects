2f436f1942c790d830ef49c1f11db627623dc9e6
Julian Mercado <julianmercadomeza@gmail.com>
2024-07-09 23:54:14 -0400

    Rename previous example and added custom name examples

---
+++ spring-boot-unit-testing/04.junit-custom-display-simple-generator-indicative-sentences/src/test/java/com/luv2code/junitdemo/DemoUtilTest.java
@@ -0,0 +1,56 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.*;
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+public class DemoUtilTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test Method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach\n");
+    }
+
+    @BeforeAll
+    static void setupBeforeEachAllMethods() {
+        System.out.println("@BeforeAll executes only before all test methods.");
+    }
+
+    static void tearDownAfterAllMethods() {
+        System.out.println("@AfterAll executes only once after all test methods");
+    }
+
+    @Test
+    void testEqualAndNotEquals() {
+
+        System.out.println("Running test: testEqualAndNotEquals");
+
+        assertEquals(6, demoUtils.add( 2, 4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+
+    }
+
+    @Test
+    void testNullAndNotNull() {
+
+        System.out.println("Running test: testNullAndNotNull");
+
+        String str1 = null;
+        String str2 = "Junit Tests";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+
+}
---
+++ spring-boot-unit-testing/07.junit-assertions/src/test/java/com/luv2code/junitdemo/DemoUtilTest.java
@@ -0,0 +1,78 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoUtilTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test Method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach\n");
+    }
+
+    @BeforeAll
+    static void setupBeforeEachAllMethods() {
+        System.out.println("@BeforeAll executes only before all test methods.");
+    }
+
+    @AfterAll
+    static void tearDownAfterAllMethods() {
+        System.out.println("@AfterAll executes only once after all test methods");
+    }
+
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void test_Equal_And_Not_Equals() {
+
+        System.out.println("Running test: testEqualAndNotEquals");
+
+        assertEquals(6, demoUtils.add( 2, 4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void test_Null_And_Not_Null() {
+
+        System.out.println("Running test: testNullAndNotNull");
+
+        String str1 = null;
+        String str2 = "Junit Tests";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+
+    @Test
+    @DisplayName("Same and Not Same")
+    void testSameAndNotSame() {
+        String str = "love2code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Object should refer to the same Object");
+        assertNotSame(str, demoUtils.getAcademy(), "Object should not refer to the same Object");
+
+    }
+
+    @Test
+    @DisplayName("True and False")
+    void testTrueAndFalse() {
+
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "gradeOne should be greater than gradeTwo");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "gradeTwo should not be greater than gradeOne");
+    }
+
+}
